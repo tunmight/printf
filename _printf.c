f3ab425bbae6413614bf3fce13c01c3f8fd92acd
GreenyNg <everodeh48@gmail.com>
2023-04-23 02:34:53 -0700

    Initial commit

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - function for format printing
+ * @format: list of arguments to printing
+ * Return: Number of characters to printing
+ */
+int _printf(const char *format, ...)
+{
+
